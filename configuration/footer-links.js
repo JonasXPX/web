25ea82839a1c4c9b7c9a4c7dab3dfcd29a100df5
JonasXPX <jonasdefarias@hotmail.com>
2017-06-20 01:43:25 -0300

    Footer links updated

--- configuration/footer-links.js
+++ configuration/footer-links.js
@@ -2,13 +2,13 @@ $(function() {
     var data = {
         footer: [{
           text: 'Como ativar um VIP',
-          url: '//www.endcraft.com.br/forum'
+          url: '//www.endcraft.com.br/forum/index.php?/topic/1984-mudan%C3%A7a-no-sistema-de-ativamento-vip/'
         },{
           text: 'Ativando o PIN',
-          url: '//www.endcraft.com.br/forum'
+          url: '//www.endcraft.com.br/forum/index.php?/topic/6588-fim-da-recupera%C3%A7%C3%A3o-via-email-novo-sistema-de-pin/'
         }, {
           text: 'Recuperando senha da conta',
-          url: '//www.endcraft.com.br/forum'
+          url: '//www.endcraft.com.br/forum/index.php?/topic/405-recupera%C3%A7%C3%A3o-de-senha/'
         }]
     };
 
