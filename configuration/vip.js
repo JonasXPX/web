0a2ac442314a07f793d0a82d3733e9780ae87462
JonasXPX <jonasdefarias@hotmail.com>
2017-06-30 02:15:59 -0300

    Atualizado página de compra de VIPs.

--- configuration/vip.js
+++ configuration/vip.js
@@ -1,8 +1,49 @@
 $(function() {
+  var lastElement = null;
+  var viptype = null;
+  $(".amount li").click(function(e){
+    if(lastElement != null)
+      $(lastElement).removeClass("selected");
+    $(this).addClass("selected");
+    lastElement = $(this);
+    updateValues($(this));
+  });
+  $(".viptypes").click(function(){
+    if(lastElement)
+      updateValues(lastElement);
+  });
+  $("#comprar").click(function(e){
+    if(!lastElement){
+      e.preventDefault();
+      $(".amount li").each(function(s){
+        $(this).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+      });
+      return;
+    }
+    $("<form method='POST' action='//www.endcraft.com.br/data/pagamento.php' target='_blank'> \
+        <input type='hidden' name='username' value='"+ Cookies.get("nickname") +"'> \
+        <input type='hidden' name='quantity' value='"+ lastElement.text() +"'> \
+        <input type='hidden' name='type' value='"+ $("[name='viptype']:checked").attr("id") +"'> \
+        <input type='hidden' name='amount' value='"+ $("[name='viptype']:checked").attr("value") +"'> \
+        <input type='hidden' name='idref' value='"+ $("[name='viptype']:checked").attr("refid") +"'> \
+        </form>").appendTo("body").submit().remove();
+  });
+
+  function updateValues(clicked) {
+    var amount = parseFloat(clicked.text());
+    var price = parseFloat($("[name='viptype']:checked").attr("value"));
+    var total = amount * price;
+    $(".total").text("Valor total: R$ " + total);
+  }
+
 
   var data = {
       vips: [{
+          amount: '32.00', //checado no PHP
+          idref: '200',
+          name: 'hero',
           title: 'VIP HERO',
+          isdefault: false,
           desc: [
             'Duração: 30 Dias',
             'Usar /mc nas cidades',
@@ -20,6 +61,10 @@ $(function() {
             'Pode usar o comando /autofix para reparar automaticamente quando a armadura quebrar',
             'Pode vender itens usando o /mercado']
           }, {
+            amount: '17.00',
+            idref: '100',
+            isdefault: true,
+            name: 'lite',
             title: "VIP+",
             desc: [
               'Duração: 20 Dias',
@@ -39,5 +84,7 @@ $(function() {
     };
 
   var hand = Handlebars.compile(document.getElementById("vips-pre").innerHTML);
+  var hand_2 = Handlebars.compile(document.getElementById("vips-list").innerHTML);
   document.getElementById("content-vips").innerHTML = hand(data);
+  document.getElementById("viptypes").innerHTML = hand_2(data);
 });
